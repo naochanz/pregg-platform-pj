be52a3ed40a475507940af285af0be2acc93aaf0
shuhei tanaka <shuhei46491@gmail.com>
2025-03-18 18:12:45 +0900

    feat: signup form
    introduced react hook form for signup form

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { ProfessionalType } from "@prisma/client";
+import { z } from "zod";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+// 入力データのバリデーション用スキーマ
+export const signupSchema = z.object({
+  name: z.string().min(1, "名前は必須です").max(50, {
+    message: "名前は50文字以内で入力してください",
+  }),
+  email: z.string().email("有効なメールアドレスを入力してください"),
+  password: z.string().min(8, "パスワードは8文字以上必要です"),
+  professionalType: z.nativeEnum(ProfessionalType, {
+    errorMap: () => ({ message: "有効な士業タイプを選択してください" }),
+  }),
+  yearOfExperience: z.number().optional(),
+});
